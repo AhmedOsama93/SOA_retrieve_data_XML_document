7169f42d7ee7f3c8081d539933538e79ae399436
AhmedOsama93 <75946833+AhmedOsama93@users.noreply.github.com>
2022-11-28 01:26:25 +0200

    DOC

--- src/XML_DOC/Book.java
+++ src/XML_DOC/Book.java
@@ -1,7 +1,5 @@
 package XML_DOC;
 
-import java.util.Date;
-
 public class Book {
     String Id;
     String Author;
--- src/XML_DOC/Main.java
+++
@@ -1,7 +0,0 @@
-package XML_DOC;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
