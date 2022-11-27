1d42c15979c8c13b8cf7f296270baf65cced4efd
AhmedOsama93 <75946833+AhmedOsama93@users.noreply.github.com>
2022-11-28 00:42:08 +0200

    Initial commit

---
+++ src/XML_DOC/Book.java
@@ -0,0 +1,48 @@
+package XML_DOC;
+
+import java.util.Date;
+
+public class Book {
+    String Id;
+    String Author;
+    String Title;
+    String Genre;
+    double Price;
+    Date Publish_Date;
+    String Description;
+
+    public Book(String id, String author, String title, String genre,
+                double price, Date publish_Date, String description) {
+        Id = id;
+        Author = author;
+        Title = title;
+        Genre = genre;
+        Price = price;
+        Publish_Date = publish_Date;
+        Description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "Id='" + Id + '\'' +
+                ", Author='" + Author + '\'' +
+                ", Title='" + Title + '\'' +
+                ", Genre='" + Genre + '\'' +
+                ", Price=" + Price +
+                ", Publish_Date=" + Publish_Date +
+                ", Description='" + Description + '\'' +
+                '}';
+    }
+}
+//<Catalogue>
+//<Book ID =”BK101”>
+//<Author>Gambardella, Matthew</Author>
+//<Title>XML Developer’s Guide</Title>
+//<Genre>Computer</Genre>
+//<Price>44.95</Price>
+//<Publish_Date>2000-11-01</ Publish_Date >
+//
+//<Description>A guide for developers on how to create applications using XML.</ Description>
+//</Book>
+//</Catalogue>
---
+++ src/XML_DOC/Main.java
@@ -0,0 +1,7 @@
+package XML_DOC;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
